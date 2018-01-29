45c3fa1b6afc83a05c9e9aa020e8bd6fe1fd508e
sa.belli <sa.belli@hotmail.it>
2018-01-29 06:39:29 -0500

    x86_cpuid_brand

--- cpuid/main.c
+++ cpuid/main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 
 //looking for the nth bit
@@ -7,18 +8,30 @@
 
 void x86_cpuid_vendor(char*);
 void x86_cpuid_features(int* ecx, int* edx);
+int x86_cpuid_brand(char*);
 
 int main() {
 	char vendor[12];
+	char brand_name[48];
 	int dest_ecx;
 	int dest_edx;
-
+	
+	memset(vendor,0,12);
+	memset(brand_name,0,48);
+	
 	x86_cpuid_vendor(vendor);
 	x86_cpuid_features(&dest_ecx, &dest_edx);
-
+	if(x86_cpuid_brand(brand_name))
+		puts("branding not supported");
+	
 	printf("CPU vendor: %s\n"
 		   "HyperThreading: %s\n"
-		   "SSE4.2: %s\n",vendor, YON(X86_FEATURE_HTT(dest_edx)),YON(X86_FEATURE_SSE_4_2(dest_ecx)));
+		   "SSE4.2: %s\n"
+			 "Brand: %s\n",
+				 vendor, 
+				 YON(X86_FEATURE_HTT(dest_edx)),
+				 YON(X86_FEATURE_SSE_4_2(dest_ecx)),
+				 brand_name);
 	
 	return 0;
 }
